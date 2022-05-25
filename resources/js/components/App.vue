29d3fbfc24810c0da7679022570b421164518c55
asahiko123 <66983728+asahiko123@users.noreply.github.com>
2022-05-25 23:03:38 +0900

    node module削除npm入れ直し、vuetify導入

---
+++ resources/js/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>{{ message }}</div>
+</template>
+
+<script>
+    import { defineComponent, ref} from "vue";
+
+    export default defineComponent({
+        setup() {
+            const message = "Hello World";
+            return {
+                message
+            };
+        }
+    });
+</script>
